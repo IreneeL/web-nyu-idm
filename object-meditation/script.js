1b4ec0cfc4b0a413a09e38a5855f75c2c5fb4520
IreneeL <yl6447@nyu.edu>
2020-03-10 14:58:49 -0400

    made changes

--- object-meditation/script.js
+++ object-meditation/script.js
@@ -1,3 +1,79 @@
-function white() {
-  
-}
+$(document).ready(function() {
+
+  $("#wp").click(function() {
+    $("body").css({
+      "background-color": "#ffffff"
+    });
+    $("#w").toggle();
+  });
+
+  $("#lemonp").click(function() {
+    $(this).attr("src", "assets/lemon.png");
+    $("body").css({
+      "background-color": "#ffffcc"
+    });
+    $('#lemon').toggle();
+  });
+
+  $("#sunsetp").click(function() {
+    $(this).attr("src", "assets/light-yellow.png");
+  });
+
+  $("#summerp").click(function() {
+    $(this).attr("src", "assets/deep-yellow.png");
+  });
+
+  $("#orangep").click(function() {
+    $(this).attr("src", "assets/orange.png");
+    $("body").css({
+      "background-color": "#ffccb3"
+    });
+  });
+
+  $("#sunp").click(function() {
+    $(this).attr("src", "assets/orange-red.png");
+  });
+
+  $("#rosep").click(function() {
+    $(this).attr("src", "assets/rose.png");
+  });
+
+  $("#skyp").click(function() {
+    $(this).attr("src", "assets/light-blue.png");
+  });
+
+// left
+
+  $("#oceanp").click(function() {
+    $(this).attr("src", "assets/deep-blue.png");
+  });
+
+  $("grassp").click(function() {
+    $(this).attr("src", "assets/grass.png");
+  });
+
+  $("#treesp").click(function() {
+    $(this).attr("src", "assets/deep-green.png");
+  });
+
+  $("#dirtp").click(function() {
+    $(this).attr("src", "assets/dirt.png");
+  });
+
+  $("#brickp").click(function() {
+    $(this).attr("src", "assets/brick walls.png");
+  });
+
+  $("#brownp").click(function() {
+    $(this).attr("src", "assets/brown.png");
+  });
+
+  $("#rainp").click(function() {
+    $(this).attr("src", "assets/gray.png");
+  });
+
+  $("#blackp").click(function() {
+    $(this).attr("src", "assets/black.png");
+  });
+
+});
